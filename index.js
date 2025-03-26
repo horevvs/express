102051731788c4a18937083b623581c07a5245e0
Horev Vladimir <vshorev@yandex.ru>
2025-03-27 00:29:37 +0700

    просто комит

--- index.js
+++ index.js
@@ -22,26 +22,48 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'static', 'index.html'))
 })
 
-app.use("/about", function (_, response) {
-    response.redirect("contact")
+// app.use("/about", function (_, response) {
+//     response.redirect("contact")
+// });
+
+app.use("/info", function (_, res) {
+    res.sendFile(path.resolve(__dirname, 'static', 'about.html'))
 });
+
+app.get("/products/:productId", function (request, response) {
+    response.send("productId: " + request.params.productId)
+  });
+
+// app.use("/info/:id", function (_, res) {
+//     res.send("productId: " + res.params["id"])
+//     // console.log(req.params.id)
+//     // res.send()
+//     // res.sendFile(path.resolve(__dirname, 'static', 'about.html'))
+// });
+
+// app.use("/info/:id", function (_, res) {
+//     if(pr)
+
+//     res.sendFile(path.resolve(__dirname, 'static', 'about.html'))
+// });
+
 
 
 app.get('/home', (req, res) => {
     res.send({ id: 6, name: "Tom" })
 })
 
-app.use("/home/foo/bar", function (_, response) {
-    response.sendStatus(404)
-});
+// app.use("/home/foo/bar", function (_, response) {
+//     response.sendStatus(404)
+// });
 
-app.use("/contact", function (_, response) {
-    response.redirect("https://tpu.ru")
-});
+// app.use("/contact", function (_, response) {
+//     response.redirect("https://tpu.ru")
+// });
 
-app.use("/contact2", function (_, response) {
-    response.redirect("https://tpu.ru")
-});
+// app.use("/contact2", function (_, response) {
+//     response.redirect("https://tpu.ru")
+// });
 
 // https://metanit.com/web/nodejs/4.13.php
 
